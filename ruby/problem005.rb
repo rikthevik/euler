0a942e4237161862999925f76d4246f8dedc8726
Erik Frederiksen <erik@noodlecake.com>
2014-01-30 22:10:18 -0600

    Slow slow slow.

--- ruby/problem005.rb
+++ ruby/problem005.rb
@@ -14,10 +14,25 @@ What is the smallest positive number that is evenly divisible by all of the numb
 def run(num)
   factors = (1..num).to_a
   for i in (2..num)
-    
+    for j in (i+1..num)
+      if j % i == 0
+        puts "removing #{i}"
+        factors.delete(i) 
+      end
+    end
+  end
+  puts factors.inspect
+  puts factors.inject(1) { |a,b| a*b }
+  num = 1
+  while 1
+    if factors.all? { |factor| num % factor == 0 }
+      puts "returning #{num}"
+      return num
+    end
+    num += 1
   end
 end
 
-assert { run 10 == 250 }
-run 20
+assert { run(10) == 2520 }
+assert { run(20) == 232792560 }
 
